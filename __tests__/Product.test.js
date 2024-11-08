7dba0e1b940946a823d550fe53a2d3302e1ac682
Minje Sung <minje813@dgu.ac.kr>
2024-11-08 19:51:29 +0900

    feat(model): Product 구매 기능 추가
    Product 별로 구매 시 재고가 떨어지도록 기능 추가
    Promotion 정보를 이름으로 가지고 있도록 변경

--- __tests__/Product.test.js
+++ __tests__/Product.test.js
@@ -6,25 +6,17 @@ describe('상품 테스트', () => {
     ['name', '콜라'],
     ['price', 1000],
     ['quantity', 10],
+    ['promotion', '탄산2+1'],
   ])('각 상품의 %s 정보를 저장하고 %s를 출력한다.', (category, expected) => {
     // 콜라, 1000, 10, 탄산2 + 1;
     if (category === 'name') expect(product.getName()).toBe(expected);
     if (category === 'price') expect(product.getPrice()).toBe(expected);
     if (category === 'quantity') expect(product.getQuantity()).toBe(expected);
+    if (category === 'promotion') expect(product.getPromotion()).toBe(expected);
   });
 
-  test('상품의 프로모션 정보가 없다면 Buy N Get 1 Free 정보를 읽는다.', () => {
-    const product = new Product('콜라', 1000, 10, '탄산3+1');
-    expect(product.getPromotion()).toBe(3);
-  });
-
-  test('상품의 프로모션 정보가 없다면 -1을 프로모션 정보로 가진다.', () => {
-    const product = new Product('콜라', 1000, 10);
-    expect(product.getPromotion()).toBe(-1);
-  });
-
-  test('상품의 프로모션 정보가 있지만 N+1이 없을 경우 -1을 프로모션 정보로 가진다.', () => {
-    const product = new Product('콜라', 1000, 10, '특별할인!');
-    expect(product.getPromotion()).toBe(-1);
+  test('상품을 구매하면 그만큼 재고가 떨어져야 한다.', () => {
+    product.buy(5);
+    expect(product.getQuantity()).toBe(5);
   });
 });
--- src/model/Product.js
+++ src/model/Product.js
@@ -1,23 +1,18 @@
-const PROMOTION_VALUE_REGEXP = /(\d+)\+1/;
-
 class Product {
   #name;
   #price;
   #quantity;
   #promotion;
 
-  constructor(name, price, quantity, promotionString = null) {
+  constructor(name, price, quantity, promotion = null) {
     this.#name = name;
     this.#price = price;
     this.#quantity = quantity;
-    this.#promotion = this.#getPromotionValue(promotionString);
+    this.#promotion = promotion;
   }
 
-  #getPromotionValue(promotionString) {
-    if (PROMOTION_VALUE_REGEXP.test(promotionString)) {
-      return Number(promotionString.match(PROMOTION_VALUE_REGEXP)[1]);
-    }
-    return -1;
+  buy(amount) {
+    this.#quantity -= amount;
   }
 
   getName() {
