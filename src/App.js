84d8039666bca7744e2d5d4546086c40610458e8
Minje Sung <minje813@dgu.ac.kr>
2024-11-10 19:08:19 +0900

    refactor(App): InputHandler.js를 App 내로 편입
    InputHandler로 적절한 인자를 주기 헷갈려, App 내로 편입
    이후 리펙토링을 통해 분리 예정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import loadFromPublic from './utils/loadFromPublic.js';
 import parseItemInput from './utils/parseItemInput.js';
 import PromotionCalculator from './utils/PromotionCalculator.js';
 import Recipt from './model/Recipt.js';
-import InputHandler from './utils/InputHandler.js';
 
 class App {
   constructor() {
@@ -23,43 +22,75 @@ class App {
       OutputView.printMessage('안녕하세요. W편의점입니다.');
       OutputView.printProducts(this.inventory);
 
-      const itemInput = await loopWhileValid(
-        InputView.readItem,
-        this.inventory
-      );
-      const itemList = parseItemInput(itemInput);
-      const recipt = new Recipt();
-
-      for (const { name, amount } of itemList) {
-        const price = this.inventory.getBasicProductInfo(name).getPrice();
-        const basicAmount = this.promotionCalculator.getBasicAmount(
-          name,
-          amount
-        );
-        const freeAmount = this.promotionCalculator.getCanFreeMore(
-          name,
-          amount
-        );
-
-        const freeMoreInput = await InputHandler.getFreeMoreInput(
-          name,
-          freeAmount
-        );
-        const notPromotionInput = await InputHandler.askNotPromotionInput(
-          name,
-          amount,
-          basicAmount,
-          this.promotionCalculator
-        );
-      }
-
-      await InputHandler.memberShipInput(recipt);
+      const itemList = await this.getItemList();
+      const recipt = await this.getPurchaseRecipt(itemList);
 
       OutputView.printRecipt(recipt);
 
-      const restartInput = await loopWhileValid(InputView.replay);
-      if (restartInput === 'N') break;
+      if (await this.askToExit()) break;
     }
+  }
+
+  async getItemList() {
+    const itemInput = await loopWhileValid(InputView.readItem, this.inventory);
+    return parseItemInput(itemInput);
+  }
+
+  async getPurchaseRecipt(itemList) {
+    const recipt = new Recipt();
+
+    for (const { name, amount } of itemList) {
+      await this.getFreeMoreInput(name, amount);
+      await this.askNotPromotionInput(name, amount);
+    }
+    await this.memberShipInput(recipt);
+
+    return recipt;
+  }
+
+  async getFreeMoreInput(name, amount) {
+    const freeAmount = this.promotionCalculator.getCanFreeMore(name, amount);
+    let userInput = 'N';
+    if (freeAmount > 0)
+      userInput = await loopWhileValid(InputView.getFreeMore, name, freeAmount);
+    this.processGetFreeMoreInput(userInput);
+  }
+
+  processGetFreeMoreInput(userInput) {
+    if (userInput === 'Y') {
+    }
+    if (userInput === 'N') {
+    }
+  }
+
+  async askNotPromotionInput(name, amount) {
+    const basicAmount = this.promotionCalculator.getBasicAmount(name, amount);
+    let userInput = 'Y';
+    if (this.promotionCalculator.getPromotionProductQuantity(name) < amount)
+      userInput = await loopWhileValid(
+        InputView.askNotPromotion,
+        name,
+        basicAmount
+      );
+    this.processNotPromotionInput(userInput);
+  }
+
+  processNotPromotionInput(userInput) {
+    if (userInput === 'Y') {
+    }
+    if (userInput === 'N') {
+    }
+  }
+
+  async memberShipInput(recipt) {
+    const userInput = await loopWhileValid(InputView.getMembership);
+    if (userInput === 'Y') recipt.calculateMembershipDiscount();
+  }
+
+  async askToExit() {
+    const restartInput = await loopWhileValid(InputView.replay);
+    if (restartInput === 'N') return true;
+    return false;
   }
 }
 
